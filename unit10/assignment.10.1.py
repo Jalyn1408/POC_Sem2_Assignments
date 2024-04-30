8b9ddd9c9fe28929bf3b05671b729938b6af8e49
Ja'Lyn Leavell <leaveja000@warren.k12.in.us>
2024-04-30 12:15:25 -0400

    updates

---
+++ unit10/assignment.10.1.py
@@ -0,0 +1,5 @@
+product = lambda a, b: a * b
+#YOUDO for quotient
+#YOUDO for exponent
+
+print(product(15, 13))
---
+++ unit10/assignment.10.3.py
@@ -0,0 +1,1 @@
+file_path = "C:\\Users\\Jalyn leavell\\Development\\POC_Sem2_Assignments\\10_Functional Programming\\03_File_Handling_2\\newfile.txt" (PUT UR OUR NAME WHERE MY NAME IS)
